1be462fe0b16cbc4d919be82260b09df720c31aa
sibmaks <sibmaks@vk.com>
2023-05-24 18:23:47 +0300

    Add icon for iron crafting table recipe

--- src/main/java/com/github/sib_energy_craft/recipes/recipe/IronCraftingTableRecipe.java
+++ src/main/java/com/github/sib_energy_craft/recipes/recipe/IronCraftingTableRecipe.java
@@ -1,23 +1,26 @@
 package com.github.sib_energy_craft.recipes.recipe;
 
+import com.github.sib_energy_craft.energy_api.utils.Identifiers;
 import com.github.sib_energy_craft.recipes.load.RecipeSerializers;
 import com.google.gson.JsonObject;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.recipe.*;
 import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @since 0.0.1
  * @author sibmaks
  */
-@AllArgsConstructor
 public class IronCraftingTableRecipe implements Recipe<Inventory> {
 
     @Getter
@@ -29,6 +32,23 @@ public class IronCraftingTableRecipe implements Recipe<Inventory> {
     @Getter
     private final Ingredient source;
     private final ItemStack output;
+    private final Item icon;
+
+    public IronCraftingTableRecipe(@NotNull Identifier id,
+                                   @NotNull String group,
+                                   @NotNull Ingredient tool,
+                                   @NotNull Ingredient source,
+                                   @NotNull ItemStack output) {
+        this.id = id;
+        this.group = group;
+        this.tool = tool;
+        this.source = source;
+        this.output = output;
+
+        var itemId = Identifiers.of("iron_crafting_table");
+        this.icon = Registries.ITEM.getOrEmpty(itemId)
+                .orElse(Items.CRAFTING_TABLE);
+    }
 
     @Override
     public boolean matches(Inventory inventory, World world) {
@@ -61,6 +81,12 @@ public class IronCraftingTableRecipe implements Recipe<Inventory> {
     @Override
     public RecipeType<?> getType() {
         return IronCraftingTableRecipeType.INSTANCE;
+    }
+
+    @NotNull
+    @Override
+    public ItemStack createIcon() {
+        return new ItemStack(icon);
     }
 
     public static class Serializer implements RecipeSerializer<IronCraftingTableRecipe> {
